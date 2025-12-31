4a1008b0818afea19f91a52a4eec1f27b803f0eb
engafi05 <eng.afi89@gmail.com>
2025-12-31 16:21:50 +0200

    Remove local api folder

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/remote/:path*',
+        destination: 'http://engafi05-001-site1.stempurl.com/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
